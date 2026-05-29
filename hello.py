1b05c744627c86142365322c1d245f66e5d1816b
Parrishcc <Pctarver@aol.com>
2026-05-28 21:04:54 -0500

    hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
 print("Hello World from GitHub Actions")
+# Challenge 1 — Math,Change hello.py
+
+print(5+10)
+
+# Challenge 2 — Variables
+name = "Parrish"
+print("Hello, " + name)
+
+# Challenge 3 — User Input
+name = input("What is your name? ")
+print("Hello, " + Parrish)
